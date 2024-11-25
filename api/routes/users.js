e867f603a59d7ac97b08b5ee69292077bd0e2b89
shalini3112 <nkshalini3112@gmail.com>
2024-11-24 19:05:01 -0700

    added validation for preferences

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,11 +3,13 @@ import express from "express";
 import { registerUser, loginUser, getUserById, updateDietPreferences } from "../controllers/users.js";
 
 import { verifyUser } from "../middleware/authorization.js";
+import {validatePreferences } from "../middleware/validation.js";
+
 
 const router = express.Router();
 
 //POST /users/register
-router.post("/register", registerUser);
+router.post("/register",validatePreferences, registerUser);
 
 //POST /users/login
 router.post("/login", loginUser);
@@ -16,6 +18,6 @@ router.post("/login", loginUser);
 router.get("/:id", verifyUser, getUserById);
 
 // PUT /users/:id
-router.put("/:id", verifyUser, updateDietPreferences);
+router.put("/:id", verifyUser,validatePreferences ,updateDietPreferences);
 
 export default router;
